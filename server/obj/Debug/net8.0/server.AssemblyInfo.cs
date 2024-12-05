be9fa0a04cb218a23e31062356e4c769c8cbcf9a
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-12-06 04:32:19 +0800

    add NuGet Packages or Dependencies in .NET

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f126521654626728456bc718468b1aa29552395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ca7933ad73fc612ad12517da592eabaf0484a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
